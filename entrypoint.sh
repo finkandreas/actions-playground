4a75373340589e327148043bcb65ad2d35192bba
Andreas Fink <finkandreas@web.de>
2025-03-06 17:46:03 +0100

    more information

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,15 @@
 
 cat <<EOF > /tmp/script.sh
 #!/bin/bash -l
-set -x
+
+#set -x
+
 $INPUT_SCRIPT
 EOF
+
+echo "input_script="
+cat /tmp/script.sh
+echo "---- end of input_script -----"
+
 chmod +x /tmp/script.sh
 /tmp/script.sh |& tee -a $GITHUB_OUTPUT
