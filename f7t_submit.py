4aab3cf8b9aeb45f307b0e037f84beb140f5da72
Andreas Fink <finkandreas@web.de>
2025-03-07 09:37:25 +0100

    bugfix

--- f7t_submit.py
+++ f7t_submit.py
@@ -1,14 +1,14 @@
 import firecrest as fc
 import os
 
-client_id = os.environ.get("INPUT_FIRECREST_CLIENT_ID", "")
-client_secret = os.environ.get("INPUT_FIRECREST_CLIENT_SECRET", "")
+client_id = os.environ.get("INPUT_FIRECREST-CLIENT-ID", "")
+client_secret = os.environ.get("INPUT_FIRECREST-CLIENT-SECRET", "")
 if client_id == "" or client_secret == "":
     print("The client-id or client-secret is empty. Please provide the firecrest credentials")
     exit(1)
 
-url = os.environ['INPUT_FIRECREST_URL']
-auth_url = os.environ['INPUT_FIRECREST_TOKEN_URL']
+url = os.environ['INPUT_FIRECREST-URL']
+auth_url = os.environ['INPUT_FIRECREST-TOKEN-URL']
 
 client =  fc.v1.Firecrest(firecrest_url=url, authorization=fc.ClientCredentialsAuth(client_id, client_secret, auth_url, min_token_validity=60))
 
